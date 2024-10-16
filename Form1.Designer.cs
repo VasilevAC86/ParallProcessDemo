5cf7ca64784f83f86bc02bb16f3056ecb44767a2
Top <Top@8-17>
2024-10-16 21:14:30 +0500

    Не получилось остановть дублирующие процессы

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,6 +33,10 @@
             this.tbInfo = new System.Windows.Forms.TextBox();
             this.lBCountDown002 = new System.Windows.Forms.Label();
             this.lbCountDown003 = new System.Windows.Forms.Label();
+            this.btnGetStatuses = new System.Windows.Forms.Button();
+            this.lbStatusTask001 = new System.Windows.Forms.Label();
+            this.lbStatusTask002 = new System.Windows.Forms.Label();
+            this.lbStatusTask003 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnCownDown
@@ -52,14 +56,14 @@
             this.lbCountDown001.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.lbCountDown001.Location = new System.Drawing.Point(265, 34);
             this.lbCountDown001.Name = "lbCountDown001";
-            this.lbCountDown001.Size = new System.Drawing.Size(30, 24);
+            this.lbCountDown001.Size = new System.Drawing.Size(50, 24);
             this.lbCountDown001.TabIndex = 1;
-            this.lbCountDown001.Text = "__";
+            this.lbCountDown001.Text = "____";
             // 
             // tbInfo
             // 
             this.tbInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.tbInfo.Location = new System.Drawing.Point(26, 357);
+            this.tbInfo.Location = new System.Drawing.Point(269, 144);
             this.tbInfo.Multiline = true;
             this.tbInfo.Name = "tbInfo";
             this.tbInfo.Size = new System.Drawing.Size(295, 72);
@@ -71,9 +75,9 @@
             this.lBCountDown002.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.lBCountDown002.Location = new System.Drawing.Point(371, 34);
             this.lBCountDown002.Name = "lBCountDown002";
-            this.lBCountDown002.Size = new System.Drawing.Size(30, 24);
+            this.lBCountDown002.Size = new System.Drawing.Size(50, 24);
             this.lBCountDown002.TabIndex = 4;
-            this.lBCountDown002.Text = "__";
+            this.lBCountDown002.Text = "____";
             // 
             // lbCountDown003
             // 
@@ -81,15 +85,60 @@
             this.lbCountDown003.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.lbCountDown003.Location = new System.Drawing.Point(495, 34);
             this.lbCountDown003.Name = "lbCountDown003";
-            this.lbCountDown003.Size = new System.Drawing.Size(30, 24);
+            this.lbCountDown003.Size = new System.Drawing.Size(50, 24);
             this.lbCountDown003.TabIndex = 5;
-            this.lbCountDown003.Text = "__";
+            this.lbCountDown003.Text = "____";
+            // 
+            // btnGetStatuses
+            // 
+            this.btnGetStatuses.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnGetStatuses.Location = new System.Drawing.Point(26, 130);
+            this.btnGetStatuses.Name = "btnGetStatuses";
+            this.btnGetStatuses.Size = new System.Drawing.Size(193, 86);
+            this.btnGetStatuses.TabIndex = 6;
+            this.btnGetStatuses.Text = "Обновить статус процесса";
+            this.btnGetStatuses.UseVisualStyleBackColor = true;
+            this.btnGetStatuses.Click += new System.EventHandler(this.btnGetStatuses_Click);
+            // 
+            // lbStatusTask001
+            // 
+            this.lbStatusTask001.AutoSize = true;
+            this.lbStatusTask001.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbStatusTask001.Location = new System.Drawing.Point(259, 94);
+            this.lbStatusTask001.Name = "lbStatusTask001";
+            this.lbStatusTask001.Size = new System.Drawing.Size(56, 20);
+            this.lbStatusTask001.TabIndex = 7;
+            this.lbStatusTask001.Text = "Status";
+            // 
+            // lbStatusTask002
+            // 
+            this.lbStatusTask002.AutoSize = true;
+            this.lbStatusTask002.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbStatusTask002.Location = new System.Drawing.Point(365, 94);
+            this.lbStatusTask002.Name = "lbStatusTask002";
+            this.lbStatusTask002.Size = new System.Drawing.Size(56, 20);
+            this.lbStatusTask002.TabIndex = 8;
+            this.lbStatusTask002.Text = "Status";
+            // 
+            // lbStatusTask003
+            // 
+            this.lbStatusTask003.AutoSize = true;
+            this.lbStatusTask003.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbStatusTask003.Location = new System.Drawing.Point(495, 94);
+            this.lbStatusTask003.Name = "lbStatusTask003";
+            this.lbStatusTask003.Size = new System.Drawing.Size(56, 20);
+            this.lbStatusTask003.TabIndex = 9;
+            this.lbStatusTask003.Text = "Status";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbStatusTask003);
+            this.Controls.Add(this.lbStatusTask002);
+            this.Controls.Add(this.lbStatusTask001);
+            this.Controls.Add(this.btnGetStatuses);
             this.Controls.Add(this.lbCountDown003);
             this.Controls.Add(this.lBCountDown002);
             this.Controls.Add(this.tbInfo);
@@ -109,6 +158,10 @@
         private System.Windows.Forms.TextBox tbInfo;
         private System.Windows.Forms.Label lBCountDown002;
         private System.Windows.Forms.Label lbCountDown003;
+        private System.Windows.Forms.Button btnGetStatuses;
+        private System.Windows.Forms.Label lbStatusTask001;
+        private System.Windows.Forms.Label lbStatusTask002;
+        private System.Windows.Forms.Label lbStatusTask003;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -4,17 +4,22 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+//using System.Reflection.Emit;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks; // для задержки в миллисекундах
 using System.Windows.Forms;
 
 namespace ParallProcessDemo
-{
+{    
     public partial class Form1 : Form
     {
-        // Перменная для хранения текущего поля вывода
-        Label currentLable;
+        Task[] tasks = new Task[3]; // Массив задач        
+        Label currentLable; // Перменная для хранения текущего поля вывода
+        // Сущность для передачи сообщения об отмене, Класс для работы с процессом
+        static CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+        // Токен - это устр-во (программный код) для авторизации            
+        static CancellationToken token = cancellationTokenSource.Token; 
         public Form1()
         {
             InitializeComponent();            
@@ -22,22 +27,42 @@ namespace ParallProcessDemo
         
         private void btnCownDown_Click(object sender, EventArgs e)
         {
+            int num = 0;
+            Task taskTmp; // Временный эл. для хранения задания
             if (currentLable == null)
             {
-                currentLable = lbCountDown001;
+                currentLable = lbCountDown001;                
             }
             else
             {
-                if (currentLable == lbCountDown001)                
-                    currentLable = lBCountDown002;                
+                if (currentLable == lbCountDown001)
+                {
+                    currentLable = lBCountDown002;
+                    num = 1;
+                }
                 else
                     if (currentLable == lBCountDown002)
-                        currentLable = lbCountDown003;                
+                    {
+                        currentLable = lbCountDown003;
+                        num = 2;
+                    }
                     else
+                    {
                         currentLable = lbCountDown001;
+                        num = 0;
+                    }
             }
+            if (tasks[num] != null)
+            {
+                taskTmp = tasks[num];
+                taskClose();
+                //tasks[num] = CountDown003(currentLable);
+                //taskTmp.Dispose(); // Завершает задачу, не разрушая данные
+            }            
+            tasks[num] = CountDown003(currentLable);
+            tasks[num].Start();
             //CountDown001(currentLable);
-            CountDown002(currentLable);
+            //CountDown002(currentLable);            
         }
         private void CountDown001(Label _label)
         {
@@ -71,6 +96,48 @@ namespace ParallProcessDemo
                 }
             } );
             task.Start(); // Запускаем экземпляр класса task
+        }
+
+        private Task CountDown003(Label _label) // Три потока будут работать параллельно
+        {               
+            // Создаём задание () - не принимает аргументы, {} - что-то делает
+            var task = Task.Run(() => taskCountDown(_label, token)); // Токен передаём в конструктор задачи
+            return task;
+        }
+
+        private void taskClose(Task task)
+        {
+            
+            cancellationTokenSource.Cancel();
+        }
+
+        private Task taskCountDown(Label _label, CancellationToken token)
+        {
+            int _start = 30;            
+            // Создаём задание () - не принимает аргументы, {} - что-то делает
+            Task task = new Task(() =>
+            {
+                while (_start > 0)
+                {
+                    if (token.IsCancellationRequested) // Если поступил запрос на завершение процесса
+                    {
+                        // MessageBox.Show($"Процесс для {_label.Name} завершён {DateTime.Now}!");
+                        tbInfo.Text = $"Процесс для {_label.Name} завершён {DateTime.Now}!";
+                        return;
+                    }
+                    _label.Text = _start.ToString();
+                    Thread.Sleep(1000); // Закрытие потока программы на время в миллисекундах
+                    _start--;
+                }
+            }, token); // Токен передаём в конструктор задачи
+            return task;
+        }
+
+        private void btnGetStatuses_Click(object sender, EventArgs e)
+        {
+            if (tasks[0] != null) lbStatusTask001.Text = tasks[0].Status.ToString();
+            if (tasks[1] != null) lbStatusTask002.Text = tasks[1].Status.ToString();
+            if (tasks[2] != null) lbStatusTask003.Text = tasks[2].Status.ToString();
         }
     }
 }
